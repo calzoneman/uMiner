1dc96d5b998d7baba1793b13f893fbbb71187023
calzoneman <calzoneman@gmail.com>
2010-09-02 17:10:42 -0400

    Added license info

--- uMiner/Logger.cs
+++ uMiner/Logger.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/**
+ * uMiner - A lightweight custom Minecraft Classic server written in C#
+ * Copyright 2010 Calvin "calzoneman" Montgomery
+ * 
+ * Licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License
+ * (see http://creativecommons.org/licenses/by-sa/3.0/, or LICENSE.txt for a full license
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
